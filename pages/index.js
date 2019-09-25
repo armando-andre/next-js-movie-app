1db1627bfc92f5831033b3dc2ef19a179447466a
Armando Calderon <armandoandrecalderon@gmail.com>
2019-09-25 13:47:58 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import Layout from '../components/MyLayout';
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awsome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}
